b0903fc6352239da92505de084e3ff16e9bd56ad
AlexDekhant <sasha.dekhant@gmail.com>
2022-10-03 14:21:21 +0300

    Update parser_test.dart

--- github_tests/test/parser_test.dart
+++ github_tests/test/parser_test.dart
@@ -3,7 +3,7 @@ import 'package:test/test.dart';
 void main() {
   test('calculate1', () {
     var a = 2 + 2;
-    expect(a, 5);
+    expect(a, 4);
   });
   test('calculate2', () {
     var a = 2 * 2;
