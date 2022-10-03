0383366c55e12118c03258de0487a6226e2ef868
AlexDekhant <sasha.dekhant@gmail.com>
2022-10-03 13:35:25 +0300

    test project fixed

---
+++ github_tests/bin/github_tests.dart
@@ -0,0 +1,5 @@
+import 'package:github_tests/github_tests.dart' as github_tests;
+
+void main(List<String> arguments) {
+  print('Hello world: ${github_tests.calculate()}!');
+}
--- test/test/test_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:test/test.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    var a = 2 + 2;
-    expect(a, 4);
-  });
-}
