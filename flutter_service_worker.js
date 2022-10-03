1d71b0bcbfa4d4fc315a52d3dbef1f0085f8da4d
Dekhant <Dekhant@users.noreply.github.com>
2022-10-03 11:10:58 +0000

    deploy: 7cbc1a7f0eaf8ad6f5d20b424ec324f6ce52bd58

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "088b73486cdb77dc7da50f918fb4311f",
-"index.html": "b7ba63171be2836eb88a151b6503bf90",
-"/": "b7ba63171be2836eb88a151b6503bf90",
+"index.html": "8f1cc7469e79b874ca516f98a48016f9",
+"/": "8f1cc7469e79b874ca516f98a48016f9",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "manifest.json": "bac014f86596425bcb80d14b8456f183",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
