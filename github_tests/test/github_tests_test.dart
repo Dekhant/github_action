a81285311e96b18d26854cab87c0c1b1e4a29811
Carapacik <Carapacik@users.noreply.github.com>
2022-10-06 13:12:38 +0000

    deploy: 6f3e60df9137443595991a5ae9230863d0396730

---
+++ github_tests/test/github_tests_test.dart
@@ -0,0 +1,12 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate3', () {
+    var a = 2 + 2;
+    expect(a, 4);
+  });
+  test('calculate4', () {
+    var a = 2 * 2;
+    expect(a, 4);
+  });
+}
---
+++ github_tests/test/parser_test.dart
@@ -0,0 +1,12 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate1', () {
+    var a = 2 + 2;
+    expect(a, 4);
+  });
+  test('calculate2', () {
+    var a = 2 * 2;
+    expect(a, 4);
+  });
+}
